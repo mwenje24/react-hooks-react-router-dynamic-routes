4cad5e62035ec79f1a9a00590ebfd52c565fce6f
Mwenje <mwenje24@gmail.com>
2022-07-22 18:56:09 +0300

    created movielist and movieshow components

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+// import the custom `useRouteMatch` hook from React Router
+import { Route, useRouteMatch } from "react-router-dom";
+import MovieShow from "./MovieShow";
 import MoviesList from "./MoviesList";
 
+
 function MoviesPage({ movies }) {
+
+  const match = useRouteMatch();
+  console.log(match);
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow />
+      </Route>
     </div>
   );
 }
